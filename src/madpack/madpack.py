dfe44f73e6549134750f9a445e015d198c4ea4ef
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-10-01 10:09:35 +0500

    fix

--- src/madpack/madpack.py
+++ src/madpack/madpack.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S python3 -E
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Main Madpack installation executable.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -1335,7 +1335,7 @@ def set_dynamic_library_path_in_database(dbver_split, madlib_library_path):
                 (portid == 'postgres' and is_rev_gte(dbver_split, get_rev_num('13.0')))):
                 libdir = libdir.decode()
 
-            libdir = libdir.strip()+'/postgresql'
+            libdir = libdir.strip().decode()+'/postgresql'
             paths.append(libdir)
 
         paths.append(madlib_library_path)
